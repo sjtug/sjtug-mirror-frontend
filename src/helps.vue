9bb75845fa22ad794fb1f9859e09cefc8cf42533
hawnzug <hawnzug@gmail.com>
2016-11-06 09:13:52 +0800

    helps and news in raw html

--- src/helps.vue
+++ src/helps.vue
@@ -1,5 +1,48 @@
 <template>
     <div>
-        This is helps!
+    <template v-for="help in helps">
+    {{ help.title }}
+    <div v-html="help.content">
+    </div>
+    <hr>
+    </template>
     </div>
 </template>
+
+<script>
+
+import { markdown } from 'markdown';
+import 'whatwg-fetch';
+
+export default {
+    data() {
+        return {
+            helps: [],
+        };
+    },
+    created() {
+        this.fetchHelps();
+    },
+    methods: {
+        fetchHelps() {
+            fetch('/data.json')
+                .then(response => response.json())
+                .then((data) => {
+                    for (const filename of data.helpfiles) {
+                        fetch(`/helps/${filename}.md`)
+                            .then(response => response.text())
+                            .then((text) => {
+                                console.log(text);
+                                this.helps.push({
+                                    title: filename,
+                                    content: markdown.toHTML(text),
+                                });
+                            });
+                    }
+                }
+            );
+        },
+    },
+};
+
+</script>
--- src/news.vue
+++ src/news.vue
@@ -1,5 +1,50 @@
 <template>
     <div>
-    This is news!
-</div>
+    <template v-for="n in news">
+    {{ n.title }}
+    {{ n.date }}
+    <div v-html="n.content">
+    </div>
+    <hr>
+    </template>
+    </div>
 </template>
+
+<script>
+
+import { markdown } from 'markdown';
+import 'whatwg-fetch';
+
+export default {
+    data() {
+        return {
+            news: [],
+        };
+    },
+    created() {
+        this.fetchNews();
+    },
+    methods: {
+        fetchNews() {
+            fetch('/data.json')
+                .then(response => response.json())
+                .then((data) => {
+                    for (const n of data.newsfiles) {
+                        fetch(`/news/${n.filename}.md`)
+                            .then(response => response.text())
+                            .then((text) => {
+                                console.log(text);
+                                this.news.push({
+                                    title: n.title,
+                                    date: n.date,
+                                    content: markdown.toHTML(text),
+                                });
+                            });
+                    }
+                }
+            );
+        },
+    },
+};
+
+</script>

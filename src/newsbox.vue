91295d7ecb0c734272420f397c3c0402cab389b9
hawnzug <hawnzug@gmail.com>
2016-12-11 16:15:39 +0800

    Add newsbox

---
+++ src/newsbox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+    <div class="card-content">
+    <router-link :to="{ name: 'newsContent', params: { title: news.filename }}">
+    {{ news.title }}
+    </router-link>
+    {{ news.date }}
+    </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['news'],
+};
+
+</script>

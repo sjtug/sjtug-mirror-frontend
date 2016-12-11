90d570dac0fc782a9f7224ad32ed2adbf65053b5
Zheng Lv <lv.zheng.2015@gmail.com>
2016-12-11 16:15:13 +0800

    add support for legacy (aka. tunasync) status API
    This commit add support for syncing status API introduced in
    tuna/tunasync#15.

--- src/mutations.js
+++ src/mutations.js
@@ -5,6 +5,16 @@ import state from './state';
 
 export default {
     updateMirrorList() {
+        if (config.statusVer === 'lug-v0') {
+            this.updateML_lugv0();
+        } else if (config.statusVer === 'legacy') {
+            this.updateML_legacy();
+        } else {
+            console.log('updateMirrorList: unknown statusVer in config');
+        }
+    },
+
+    updateML_lugv0() {
         const fetched = fetch(config.statusUrl).then(response => response.json());
         fetched.catch((error) => {
             console.log(error.message);
@@ -41,6 +51,43 @@ export default {
             });
         });
     },
+
+    updateML_legacy() {
+        const fetched = fetch(config.statusUrl).then(response => response.json());
+        fetched.catch((error) => {
+            console.log(error.message);
+        });
+        fetched.then((j) => {
+            state.mirrorList.splice(0);
+            j.forEach((repo) => {
+                let statusClass = '';
+                let status = '';
+                if (repo.status === 'success') {
+                    statusClass = 'sync-finished';
+                    status = 'Idle'; // Hidden text
+                } else if (repo.status === 'syncing') {
+                    statusClass = 'sync-working';
+                    status = 'Syncing';
+                } else if (repo.status === 'failed') {
+                    statusClass = 'sync-failed';
+                    status = 'Failed';
+                } else {
+                    statusClass = 'sync-failed';
+                    status = 'Unknown';
+                }
+                // prettify the date string
+                const lastSync = repo.last_update;
+                state.mirrorList.push({
+                    name: repo.name,
+                    display_name: repo.name,
+                    last_sync: lastSync,
+                    status_class: statusClass,
+                    status,
+                });
+            });
+        });
+    },
+
     fetchHelps() {
         fetch('/data.json')
             .then(response => response.json())

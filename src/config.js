d2a20dbdd59f9c495149c27c0245896f32fa0534
Vic Luo <vicluo96@gmail.com>
2017-06-13 00:23:26 +0800

    config: use local mocks

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
     // statusUrl: 'http://localhost:7001/lug/v1/manager',
     // statusVer: 'lug-v0',
-    statusUrl: 'https://mirrors.sjtug.org/static/tunasync.json',
-    statusVer: 'legacy',
+    statusUrl: 'https://htfy96.github.io/sjtug-mirror-frontend/mock/manager.json',
+    statusVer: 'lug-v0',
     // mirrorBaseUrl: 'http://localhost:8000/', // don't miss the terminating slash
     mirrorBaseUrl: 'https://mirrors.sjtug.org/', // don't miss the terminating slash
     updateInterval: 30000,

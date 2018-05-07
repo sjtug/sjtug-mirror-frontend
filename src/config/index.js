9252a97b4d7c68606d3f645a7cbb61ecccd41d5e
Vic Luo <vicluo96@gmail.com>
2018-05-07 14:42:05 +0800

    config: update backend address

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ export default {
   // Address of help articles
   helpAddress: 'https://sjtug-portal-1251836446.file.myqcloud.com/tags/mirror-help/index.xml',
   // All packages should be accessible at backendAddressBase + '/' + repo.name
-  backendAddressBase: 'https://mirrors.sjtug.org',
+  backendAddressBase: 'https://mirrors.sjtug.sjtu.edu.cn',
   // Address of the summary of backend in sjtug/lug format
-  backendSummaryAddress: 'https://mirrors.sjtug.org/lug/v1/manager/summary'
+  backendSummaryAddress: 'https://mirrors.sjtug.sjtu.edu.cn/lug/v1/manager/summary'
 }

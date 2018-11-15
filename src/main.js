ffe79d2c00f7b1f398b65dc7b906dfb29660f23c
Zheng Luo <vicluo96@gmail.com>
2018-11-14 17:03:40 -0800

    add sentry for remote error monitoring

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import '@/style/site.scss'
 import 'font-awesome/css/font-awesome.min.css'
 import App from './App'
 import router from './router'
+import * as Sentry from '@sentry/browser'
 
 Vue.config.productionTip = false
+
+Sentry.init({
+  dsn: 'https://cb947836e1a74618910282917ca66e3d@sentry.io/1323290',
+  integrations: [new Sentry.Integrations.Vue({ Vue })]
+})
 
 /* eslint-disable no-new */
 new Vue({

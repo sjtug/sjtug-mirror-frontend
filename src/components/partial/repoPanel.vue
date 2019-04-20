eb19621c98f25dca93da1dab6e47a3218a73c42c
SkyZH <iskyzh@gmail.com>
2019-04-20 11:25:52 +0800

    new style of releasePanel

--- src/components/partial/repoPanel.vue
+++ src/components/partial/repoPanel.vue
@@ -2,9 +2,6 @@
 <div>
   <div class="level">
     <div class="level-left">
-      <h2 class="subtitle">镜像&#x2F;反代列表</h2>
-    </div>
-    <div class="level-right">
       <div class="field">
         <p class="control has-icons-left">
         <input v-model="filterInput" class="input" type="text" placeholder="输入名称查找镜像/反代服务">

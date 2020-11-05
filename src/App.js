c7f1dbb62c16099bcfc899a0a0aeb53081942505
Alex Chi <iskyzh@gmail.com>
2020-11-05 18:13:56 +0800

    format

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.scss';
-import useData from './utils';
-import MirrorList from './MirrorList';
-import mapValues from 'lodash/mapValues';
-import assign from 'lodash/assign';
+import "./App.scss";
+import useData from "./utils";
+import MirrorList from "./MirrorList";
+import mapValues from "lodash/mapValues";
+import assign from "lodash/assign";
 
 function transform(status, base, server) {
   return mapValues(status, (v, k) => ({
@@ -10,18 +10,30 @@ function transform(status, base, server) {
     lastFinished: v.LastFinished,
     result: v.Result,
     url: `${base}/${k}/`,
-    server: server
-  }))
+    server: server,
+  }));
 }
 
 function App() {
-  const { data: summary1 } = useData("http://mirrors.internal.skyzh.xyz/lug/v1/manager/summary");
-  const { data: summary2 } = useData("https://mirrors.sjtug.sjtu.edu.cn/lug/v1/manager/summary");
+  const { data: summary1 } = useData(
+    "http://mirrors.internal.skyzh.xyz/lug/v1/manager/summary"
+  );
+  const { data: summary2 } = useData(
+    "https://mirrors.sjtug.sjtu.edu.cn/lug/v1/manager/summary"
+  );
   if (summary1 || summary2) {
-    const summary1t = transform((summary1 || {}).WorkerStatus || {}, "http://mirrors.internal.skyzh.xyz", "Siyuan");
-    const summary2t = transform((summary2 || {}).WorkerStatus || {}, "https://mirrors.sjtug.sjtu.edu.cn", "Zhiyuan");
+    const summary1t = transform(
+      (summary1 || {}).WorkerStatus || {},
+      "http://mirrors.internal.skyzh.xyz",
+      "Siyuan"
+    );
+    const summary2t = transform(
+      (summary2 || {}).WorkerStatus || {},
+      "https://mirrors.sjtug.sjtu.edu.cn",
+      "Zhiyuan"
+    );
     const summary = summary2t;
-    console.log(summary1t, summary2t)
+    console.log(summary1t, summary2t);
     assign(summary, summary1t);
 
     return (
@@ -29,7 +41,9 @@ function App() {
         <div className="jumbotron jumbotron-fluid bg-light px-0 pt-0 bg-sjtug text-white">
           <nav className="navbar navbar-dark">
             <div className="container">
-              <a className="navbar-brand" href="/">SJTUG 软件源镜像服务</a>
+              <a className="navbar-brand" href="/">
+                SJTUG 软件源镜像服务
+              </a>
             </div>
           </nav>
           <div className="container pt-5">
@@ -44,17 +58,21 @@ function App() {
           <div className="container">
             <div className="col-md-6 p-1">
               <p>
-                您正在使用 SJTUG Siyuan 服务器。
-                该服务器托管于 jCloud，由 <a href="https://sjtug.org">上海交通大学 Linux 用户组 (SJTUG)</a> 维护，
-                由 <a href="https://net.sjtu.edu.cn">上海交通大学网络信息中心</a> 赞助资源。
-            </p>
+                您正在使用 SJTUG Siyuan 服务器。 该服务器托管于 jCloud，由{" "}
+                <a href="https://sjtug.org">
+                  上海交通大学 Linux 用户组 (SJTUG)
+                </a>{" "}
+                维护， 由{" "}
+                <a href="https://net.sjtu.edu.cn">上海交通大学网络信息中心</a>{" "}
+                赞助资源。
+              </p>
             </div>
           </div>
         </footer>
       </>
     );
   } else {
-    return <div></div>
+    return <div></div>;
   }
 }
 
--- src/MirrorList.js
+++ src/MirrorList.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import * as timeago from 'timeago.js';
-import sortBy from 'lodash/sortBy';
+import React from "react";
+import * as timeago from "timeago.js";
+import sortBy from "lodash/sortBy";
 
 function SvgCheck() {
-    return <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      className="bi bi-check"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
+      />
     </svg>
+  );
 }
 
 function SvgX() {
-    return <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      className="bi bi-x"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+      />
     </svg>
+  );
 }
 
 const STATUS_SUCCESS = 0;
@@ -21,60 +45,73 @@ const STATUS_FAILED = 2;
 const SYNC_THRESHOLD = 86400 * 1000 * 300;
 
 function getStatus(value) {
-    if (!value.idle) {
-        return STATUS_SYNC;
-    }
-    if (!value.result) {
-        return STATUS_FAILED;
-    }
-    if (Date.now() - (new Date(value.lastFinished)).getTime() > SYNC_THRESHOLD) {
-        return STATUS_SYNC;
-    }
-    return STATUS_SUCCESS;
+  if (!value.idle) {
+    return STATUS_SYNC;
+  }
+  if (!value.result) {
+    return STATUS_FAILED;
+  }
+  if (Date.now() - new Date(value.lastFinished).getTime() > SYNC_THRESHOLD) {
+    return STATUS_SYNC;
+  }
+  return STATUS_SUCCESS;
 }
 
 function MirrorList({ summary }) {
-    const rows = sortBy(Object.keys(summary)).map(key => {
-        const value = summary[key];
-        const status = getStatus(value)
-        return <tr key={key}>
-            <td><a href={value.url}>{key}</a></td>
-            <td>{Date.now() - (new Date(value.lastFinished)).getTime() > SYNC_THRESHOLD ? "未知" : timeago.format(value.lastFinished, 'zh_CN')}</td>
-            <td>
-                <div className="d-flex flex-row align-items-center">
-                    {status === STATUS_SUCCESS ?
-                        <>
-                            <div className="mx-1 text-success"><SvgCheck /></div>
-                            <div className="text-success">同步成功</div>
-                        </>
-                        :
-                        (status === STATUS_FAILED ?
-                            <>
-                                <div className="mx-1 text-warning"><SvgX /></div>
-                                <div className="text-warning">同步失败</div>
-                            </>
-                            : <>
-                                <div className="spinner-grow spinner-grow-sm mx-1 text-info"></div>
-                                <div className="text-info">正在同步</div>
-                            </>)
-                    }
-
+  const rows = sortBy(Object.keys(summary)).map((key) => {
+    const value = summary[key];
+    const status = getStatus(value);
+    return (
+      <tr key={key}>
+        <td>
+          <a href={value.url}>{key}</a>
+        </td>
+        <td>
+          {Date.now() - new Date(value.lastFinished).getTime() > SYNC_THRESHOLD
+            ? "未知"
+            : timeago.format(value.lastFinished, "zh_CN")}
+        </td>
+        <td>
+          <div className="d-flex flex-row align-items-center">
+            {status === STATUS_SUCCESS ? (
+              <>
+                <div className="mx-1 text-success">
+                  <SvgCheck />
                 </div>
-            </td>
-            <td className="small">{`存储@${value.server}`}</td>
+                <div className="text-success">同步成功</div>
+              </>
+            ) : status === STATUS_FAILED ? (
+              <>
+                <div className="mx-1 text-warning">
+                  <SvgX />
+                </div>
+                <div className="text-warning">同步失败</div>
+              </>
+            ) : (
+              <>
+                <div className="spinner-grow spinner-grow-sm mx-1 text-info"></div>
+                <div className="text-info">正在同步</div>
+              </>
+            )}
+          </div>
+        </td>
+        <td className="small">{`存储@${value.server}`}</td>
+      </tr>
+    );
+  });
+  return (
+    <table className="table table-sm table-borderless">
+      <thead>
+        <tr>
+          <th scope="col">镜像名称</th>
+          <th scope="col">上次同步</th>
+          <th scope="col">状态</th>
+          <th scope="col">类型</th>
         </tr>
-    })
-    return <table className="table table-sm table-borderless">
-        <thead>
-            <tr>
-                <th scope="col">镜像名称</th>
-                <th scope="col">上次同步</th>
-                <th scope="col">状态</th>
-                <th scope="col">类型</th>
-            </tr>
-        </thead>
-        <tbody>{rows}</tbody>
+      </thead>
+      <tbody>{rows}</tbody>
     </table>
+  );
 }
 
 export default MirrorList;

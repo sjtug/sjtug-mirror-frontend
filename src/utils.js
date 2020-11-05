0877e88c9e02df44ae2dbe3e2bd333ed9319ec9b
Alex Chi <iskyzh@gmail.com>
2020-11-05 19:32:32 +0800

    feat: add docs support

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,25 @@
 import { get } from "axios";
 import useSWR from "swr";
+import Parser from "rss-parser";
 
 export const fetcher = (...args) =>
   get(...args)
     .then((resp) => resp.data)
     .catch((error) => console.log(error.toJSON()));
+
+export const rssFetcher = (...args) =>
+  get(...args)
+    .then((resp) => resp.data)
+    .then((resp) => {
+      const parser = new Parser();
+      return Promise.resolve(parser.parseString(resp));
+    })
+    .catch((error) => console.log(error.toJSON()));
+
 export default function useData(url) {
   return useSWR(url, fetcher);
+}
+
+export function useRSSData(url) {
+  return useSWR(url, rssFetcher);
 }

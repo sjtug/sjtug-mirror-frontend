8bfa00d5eb9f6ae9195ed3cb617cfaad6aa31980
hebingchang <hebingchang1@live.com>
2020-12-19 22:07:32 +0800

    ui: return to top after route change (and preserve the position when going back) (#53)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Docs from "./Docs";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/docs">
-            <Docs />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-      <Footer></Footer>
+      <ScrollToTop>
+        <div>
+          <Switch>
+            <Route path="/docs">
+              <Docs />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+        <Footer></Footer>
+      </ScrollToTop>
     </Router>
   );
 }
